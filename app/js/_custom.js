962c98a4e814903366434ecc1f82f212f80054ae
Andrew Ilin <aniki.iljin@gmail.com>
2019-11-12 13:19:51 +0500

    sfsd

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,7 +1,22 @@
-$(function() {
+$(function () {
 
    $('#carousel-main').slick({
+      infinite: false
+   });
 
-});
-   
+   $('#carousel-pizza').slick({
+      infinite: false,
+      slidesToShow: 3,
+      slidesToScroll: 2,
+      dots: true,
+   });
+
+   $('#carousel-snack').slick({
+      infinite: false,
+      slidesToShow: 3,
+      slidesToScroll: 2,
+      dots: true,
+   });
+
+
 });

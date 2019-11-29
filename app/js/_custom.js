0278a6d48d820f69fd34ac085f7fb25c10c8bda3
Andrew Ilin <aniki.iljin@gmail.com>
2019-11-29 21:16:46 +0500

    sdfsdf

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,7 +1,21 @@
 $(function () {
 
    $('#carousel-main').slick({
-      infinite: false
+      infinite: false,
+      responsive: [
+         {
+            breakpoint: 992,
+            settings: {
+               dots: true
+            }
+         },
+         {
+            breakpoint: 576,
+            settings: {
+               arrows: false
+            }
+         }
+      ]
    });
 
    $('#carousel-pizza').slick({
@@ -9,6 +23,30 @@ $(function () {
       slidesToShow: 3,
       slidesToScroll: 2,
       dots: true,
+      responsive: [
+         {
+            breakpoint: 1200,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 1,
+            }
+         },
+         {
+            breakpoint: 992,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+            }
+         },
+         {
+            breakpoint: 576,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+               arrows: false
+            }
+         }
+      ]
    });
 
    $('#carousel-snack').slick({
@@ -16,6 +54,22 @@ $(function () {
       slidesToShow: 3,
       slidesToScroll: 2,
       dots: true,
+      responsive: [
+         {
+            breakpoint: 1200,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 1
+            }
+         },
+         {
+            breakpoint: 992,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1
+            }
+         }
+      ]
    });
 
    $('#carousel-drink').slick({
@@ -24,7 +78,27 @@ $(function () {
       slidesToScroll: 1,
       initialSlide: 1,
       centerPadding: '0px',
-      centerMode: true
+      centerMode: true,
+      responsive: [
+         {
+            breakpoint: 992,
+            settings: {
+               slidesToShow: 2,
+               centerMode: false,
+               initialSlide: 0,
+               dots: true
+            }
+         },
+         {
+            breakpoint: 576,
+            settings: {
+               slidesToShow: 1,
+               centerMode: false,
+               initialSlide: 0,
+               dots: true
+            }
+         }
+      ]
    });
 
 

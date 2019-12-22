ffcc35ef3f280369fabe9b2bd5ebd53a3d225125
devandreyilin <dev.andreyilin@gmail.com>
2019-12-22 08:09:29 +0500

    dsfsdf

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -12,7 +12,8 @@ $(function () {
          {
             breakpoint: 576,
             settings: {
-               arrows: false
+               arrows: false,
+               dots: true
             }
          }
       ]
@@ -68,6 +69,14 @@ $(function () {
                slidesToShow: 1,
                slidesToScroll: 1
             }
+         },
+         {
+            breakpoint: 576,
+            settings: {
+               arrows: false,
+               slidesToShow: 1,
+               slidesToScroll: 1
+            }
          }
       ]
    });
@@ -99,6 +108,44 @@ $(function () {
             }
          }
       ]
+   });
+
+   // Настройка размеров пиццы
+   $('.carousel-pizza .size-price-wrap .size').on('click', function() {
+      // Удаление класса active со всех элементов
+      $(this).parent().children('.size').removeClass('active');
+
+      // Добавление/Удаление класса active с элемента карусели
+      if(!$(this).hasClass('active')) {
+         $(this).addClass('active');
+
+      } else if($(this).hasClass('active')) {
+         $(this).removeClass('active');
+      }
+
+      // Изменение стоимости
+      let price = +$($(this).parents('.size-price-wrap').find('.sum')).attr('data-price');
+      let size = +$($(this).parents('.size-price-wrap').find('.size.active')).attr('data-size');
+      let totalPrice = Math.ceil(price + (price * 0.15));
+
+      // Функция для соблюдения пробелов у сумм более тысячи
+      function toNiceView(n) {
+         n += "";
+         n = new Array(4 - n.length % 3).join("U") + n;
+         return n.replace(/([0-9U]{3})/g, "$1 ").replace(/U/g, "");
+      }
+
+      // Перезапись переменных
+      price = toNiceView(price);
+      totalPrice = toNiceView(totalPrice);
+
+      // Присваивание цены, в зависимости от размера
+      if(size === 32) {
+         $(this).parents('.size-price-wrap').find('.sum').text(totalPrice);
+      } else if (size === 26) {
+         $(this).parents('.size-price-wrap').find('.sum').text(price);
+      }
+
    });
 
 
